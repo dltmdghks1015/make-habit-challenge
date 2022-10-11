200955529aedf4efd6ca9f9974b910faba3d8df6
IpYo <81682346+dltmdghks1015@users.noreply.github.com>
2022-10-11 16:43:54 +0900

    44
    44

---
+++ C# 반복문 구구단/WindowsFormsApp1/Form5.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(333, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(419, 37);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "구구단 전체 출력 (for each 문)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(162, 156);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(770, 467);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(242, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(637, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "구구단 전체를 3열로 보여드립니다. 버튼만 눌러 주세요";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 656);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ C# 반복문 구구단/WindowsFormsApp1/Form5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] i = { 1, 4, 7 };
+            int[] j = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] k = { 0, 1, 2 };
+
+            foreach (int num1 in i)
+            {
+                foreach (int num2 in j)
+                {
+                    foreach (int num3 in k)
+                    {
+                        textBox1.Text = textBox1.Text + (num1 + num3) + " X " + num2 + " = " + ((num1 + num3) * num2) + "          ";
+                    }
+                    textBox1.Text = textBox1.Text + Environment.NewLine;
+                }
+                textBox1.Text = textBox1.Text + Environment.NewLine;
+            }
+        }
+    }
+}
