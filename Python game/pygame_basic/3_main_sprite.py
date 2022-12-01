580c31175fa6783bf40c96bedea22edbbc5835c4
IpYo <81682346+dltmdghks1015@users.noreply.github.com>
2022-12-01 21:42:23 +0900

    89
    89

--- Python game/pygame_basic/3_main_sprite.py
+++ Python game/pygame_basic/3_main_sprite.py
@@ -15,12 +15,11 @@ background = pygame.image.load("E:/Python game/pygame_basic/background.png")
 
 # 캐릭터(스프라이트) 불러오기
 character = pygame.image.load("E:/Python game/pygame_basic/character.png")
-character_size = character.get.rect().size # 이미지의 크기를 구해옴
+character_size = character.get_rect().size # 이미지의 크기를 구해옴
 character_width = character_size[0] # 캐릭터의 가로 크기
 character_height = character_size[1] # 캐릭터의 세로 크기
-character_x_pos = (screen_width / 2) - (character_width / 2) # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로)
+character_x_pos = (screen_width / 2) - (character_width / 2)# 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로)
 character_y_pos = screen_height - character_height # 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로)
-
 
 # 이벤트 루프
 running = True # 게임이 진행중인가?
@@ -32,7 +31,7 @@ while running:
     screen.blit(background, (0, 0)) # 배경 그리기 
 
     screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
-    
+
     pygame.display.update() # 게임화면을 다시 그리기
 
 # pygame 종료
